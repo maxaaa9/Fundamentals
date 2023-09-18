812588b0adbe569e46291e42bec8b54b84b44634
Angel Karasotyanov <maxaaa9@abv.bg>
2023-09-18 21:21:12 +0300

    Exercise 2

--- Fundamentals/15.09 - Ivan Shopov/more_exercises/find_the_capitals.py
+++ Fundamentals/15.09 - Ivan Shopov/more_exercises/find_the_capitals.py
@@ -2,11 +2,8 @@ word = input()
 
 my_list = []
 
-for index, i in enumerate(word):
-    if i.isupper():
+for index, char in enumerate(word):
+    if char.isupper():
         my_list.append(index)
 
 print(my_list)
-
-
-
