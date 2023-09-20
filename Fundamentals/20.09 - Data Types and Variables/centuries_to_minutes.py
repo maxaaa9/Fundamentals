3b6d643f556c771f08b280804e98454042b3c075
Angel Karasotyanov <maxaaa9@abv.bg>
2023-09-20 22:00:20 +0300

    Exercise 1

---
+++ Fundamentals/20.09 - Data Types and Variables/centuries_to_minutes.py
@@ -0,0 +1,8 @@
+centuries = int(input())
+
+years = 100 * centuries
+days = int((365.2422) * years)
+hours = 24 * days
+minutes = 60 * hours
+
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
---
+++ Fundamentals/20.09 - Data Types and Variables/pounds_to_dollars.py
@@ -0,0 +1,7 @@
+pounds = int(input())
+
+pound_to_dollar = 1.31
+
+dollar = pounds * pound_to_dollar
+
+print(f"{dollar:.3f}")
