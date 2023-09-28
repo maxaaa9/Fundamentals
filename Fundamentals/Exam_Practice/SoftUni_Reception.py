286c63c86702e6ef756306fb1f392169878664d7
Angel Karasotyanov <maxaaa9@abv.bg>
2023-09-28 22:47:47 +0300

    Exam 1 - Exercise 1

--- Fundamentals/Exam_Practice/SoftUni_Reception.py
+++ Fundamentals/Exam_Practice/SoftUni_Reception.py
@@ -3,9 +3,12 @@ first_employee = int(input())
 second_employee = int(input())
 third_employee = int(input())
 total_students = int(input())
-
 total_capacity_of_employee = first_employee + second_employee + third_employee
-needed_hour = total_students / total_capacity_of_employee
-needed_hour = ceil(needed_hour)
-needed_hour += needed_hour // 4
-print(f"Time needed: {needed_hour}h.")
+hours_counter = 0
+while total_students > 0:
+    total_students -= total_capacity_of_employee
+    hours_counter += 1
+    if hours_counter % 4 == 0:
+        hours_counter += 1
+
+print(f"Time needed: {hours_counter}h.")
