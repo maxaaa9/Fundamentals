f92217678a5b1ba08392907a6dda9d5f75d1a3ac
Angel Karasotyanov <maxaaa9@abv.bg>
2023-10-01 13:25:45 +0300

    Exercise 7 - additional task

---
+++ Fundamentals/29.09 - List Basics - exercise/easter_gift.py
@@ -0,0 +1,28 @@
+names_of_gifts = input().split()
+command = input()
+
+while command != "No Money":
+    gifts = command.split()
+    status = gifts[0]
+    product = gifts[1]
+    if len(gifts) >= 3:
+        quantity = gifts[2]
+        quantity = int(quantity)
+        if status == "Required":
+            if 0 <= quantity < len(names_of_gifts):
+                names_of_gifts[quantity] = product
+    if status == "OutOfStock":
+        for check in range(len(names_of_gifts)):
+            if names_of_gifts[check] == product:
+                names_of_gifts[check] = "None"
+    elif status == "JustInCase":
+        names_of_gifts.pop()
+        names_of_gifts.append(product)
+    command = input()
+while 'None' in names_of_gifts:
+    names_of_gifts.remove("None")
+
+my_string_output = " ".join(names_of_gifts)
+
+print(my_string_output)
+
