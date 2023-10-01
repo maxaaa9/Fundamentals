d9404a45ed9eb8dd607d6e968ba829b820560a08
Angel Karasotyanov <maxaaa9@abv.bg>
2023-10-01 13:37:39 +0300

    Exercise 7

--- Fundamentals/29.09 - List Basics - exercise/easter_gift.py
+++ Fundamentals/29.09 - List Basics - exercise/easter_gift.py
@@ -5,19 +5,20 @@ while command != "No Money":
     gifts = command.split()
     status = gifts[0]
     product = gifts[1]
-    if len(gifts) >= 3:
-        quantity = gifts[2]
-        quantity = int(quantity)
-        if status == "Required":
-            if 0 <= quantity < len(names_of_gifts):
-                names_of_gifts[quantity] = product
-    if status == "OutOfStock":
-        for check in range(len(names_of_gifts)):
-            if names_of_gifts[check] == product:
-                names_of_gifts[check] = "None"
-    elif status == "JustInCase":
-        names_of_gifts.pop()
-        names_of_gifts.append(product)
+    if product in names_of_gifts or status != "OutOfStock":
+        if len(gifts) >= 3:
+            quantity = gifts[2]
+            quantity = int(quantity)
+            if status == "Required":
+                if len(names_of_gifts) > quantity >= 0:
+                    names_of_gifts[quantity] = product
+        if status == "OutOfStock":
+            for check in range(len(names_of_gifts)):
+                if names_of_gifts[check] == product:
+                    names_of_gifts[check] = "None"
+        elif status == "JustInCase":
+            names_of_gifts.pop()
+            names_of_gifts.append(product)
     command = input()
 while 'None' in names_of_gifts:
     names_of_gifts.remove("None")
@@ -25,4 +26,3 @@ while 'None' in names_of_gifts:
 my_string_output = " ".join(names_of_gifts)
 
 print(my_string_output)
-
