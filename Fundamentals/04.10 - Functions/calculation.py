4c351f50b16285681b8a47586b7fa0c84f15c55d
maxaaa9 <maxaaa9@abv.bg>
2023-10-07 18:13:27 +0300

    Exercise 3

---
+++ Fundamentals/04.10 - Functions/calculation.py
@@ -0,0 +1,20 @@
+def calculator (first_integer, action, second_integer) -> int:
+    first_integer = int(first_integer)
+    second_integer = int(second_integer)
+    result = 0
+    if action == "multiply":
+        result = first_integer * second_integer
+    elif action == "divide":
+        result = first_integer / second_integer
+    elif action == "add":
+        result = first_integer + second_integer
+    elif action == "subtract":
+        result = first_integer - second_integer
+
+    return int(result)
+
+
+operator = input()
+integer_one = input()
+integer_two = input()
+print(calculator(integer_one, operator, integer_two))
