834ce28b36634cfb0068a49a34d8818d272052f0
maxaaa9 <maxaaa9@abv.bg>
2023-11-18 22:32:09 +0200

    Exercise 4

---
+++ Fundamentals/17.11 - RegEx - exericese/find_ocurrences_of _word_in_sentence.py
@@ -0,0 +1,11 @@
+# Write a program that finds how many times a word is used in a string.
+# The output is a single number indicating the number of times the string contains the word.
+# Note that letter case does not matter – it is case-insensitive.
+
+import re
+
+text = input()
+find_word = input()
+pattern = fr"(?i)\b({find_word})+\b"
+match = re.findall(pattern, text)
+print(len(match))
