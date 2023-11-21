361931af07adde41beca78c2d2153a1989533e15
Angel Karasotyanov <maxaaa9@abv.bg>
2023-11-21 21:50:42 +0200

    Exercise 5

--- Fundamentals/17.11 - RegEx - exericese/furniture.py
+++ Fundamentals/17.11 - RegEx - exericese/furniture.py
@@ -1,3 +1,11 @@
+# Write a program that calculates the total cost of bought furniture.
+# You will be given information about each purchase on separate lines until you receive the command "Purchase".
+# Valid information should be in the format: ">>{furniture_name}<<{price}!{quantity}".
+# The price could be a floating-point or integer number.
+# You should store the names of the furniture and the total price.
+# In the end, print the name of each bought furniture and the total cost, formatted to the second decimal point:
+
+
 import re
 
 patters = fr">>([A-Za-z]+)<<(\d+(\.[0-9]+)?)!(\d+)\b"
